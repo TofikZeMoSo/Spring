1fcb7f7df71119af78f56d37c547b08f8f3f8600
TofikZeMoSo <tofik.maniyar@zemosolabs.com>
2022-04-08 11:31:38 +0530

    Hibernate Advance Mapping OneToOne

---
+++ Hibernate_One_To_One_Uni/src/main/java/com/demo/JDBC/TestJDBC.java
@@ -0,0 +1,27 @@
+package com.demo.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+
+    public static void main(String[] args) {
+
+            String jdbcUrl = "jdbc:mysql://localhost:3306/OneToOne?allowPublicKeyRetrieval=true&useSSL=false";
+            String user = "OneToOne";
+            String pass = "Tofik@0143";
+
+            try {
+                System.out.println("Connecting to database: "+jdbcUrl);
+
+                Connection myConn = DriverManager.getConnection(jdbcUrl,user,pass);
+
+                System.out.println("Connection is SuccessFull.....");
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
---
+++ Hibernate_One_To_One_Uni/src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Spring MVC Demo- Home Page </h2>
+
+<hr>
+<a href="hello/showForm" style="color:red;"> form </a> <br>
+<a href="student/showForm" style="color:blue;"> Student form </a>
+</body>
+
+</html>
